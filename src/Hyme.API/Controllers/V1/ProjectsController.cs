cc70dda3b9b8ae67312a9048054637edf98a9da8
arjaymaligaya297@gmail.com <arjaymaligaya297@gmail.com>
2023-07-19 22:45:03 +0800

    Add Projects Controller

---
+++ src/Hyme.API/Controllers/V1/ProjectsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hyme.API.Controllers.V1
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ProjectsController()
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Get Projects
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetProjects()
+        {
+            return await Task.FromResult(Ok());
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id">Project Id</param>
+        /// <returns></returns>
+        [HttpGet("{id}", Name = "GetProjectById")]
+        public async Task<IActionResult> GetProjectById(Guid id)
+        {
+            return await Task.FromResult(Ok());
+        }
+
+    }
+}
